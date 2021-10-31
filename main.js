5ae6e890bb7783910ddd09843a8f129be606781a
huangxinpeng <gp.3098@qq.com>
2021-10-31 19:58:55 +0800

    增加创建操作符of

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import Rxjs, { map, of } from 'rxjs';
 let d$ = new Rxjs.Observable((observer) => {
   observer.next(12);
-	// observer.error(new Error('error 12'))
+  // observer.error(new Error('error 12'))
 });
 d$.subscribe({
   next: (val) => {
@@ -10,4 +10,13 @@ d$.subscribe({
   // error: (val) => {
   //   console.error(val);
   // },
+});
+
+let a$ = of([1, 2, 3, 4, 5]);
+a$.subscribe((val) => {
+  console.log('of:', val);
+});
+let a1$ = of(1, 2, 3, 4, 5, 6);
+a1$.subscribe((val) => {
+  console.log('a1$', val);
 });
