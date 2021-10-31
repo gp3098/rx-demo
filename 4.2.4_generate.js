ce51330bc616841509542fe1aedd4668a7a8aa84
huangxinpeng <gp.3098@qq.com>
2021-10-31 20:27:30 +0800

    增加generate的样例

--- 4.2.4_generate.js
+++ 4.2.4_generate.js
@@ -41,3 +41,35 @@ const range3$ = generate({
 
 console.log('generate的创建模式函数形式已经被废弃了，需要该用选项形式');
 range3$.subscribe(console.log);
+/**
+ * 在传统的js编程中，如果某个问题的解决方法是用一个for循环产生的数据集合，那么搬到rxjs世界内就是适合于使用generate来产生一个Observable对象
+ */
+const source$ = generate({
+  initialState: 'x',
+  condition(val) {
+    return val.length <= 3;
+  },
+  iterate(val) {
+    return val + 'x';
+  },
+  resultSelector(val) {
+    return val;
+  },
+});
+console.log('通过generate产生字符串');
+source$.subscribe(console.log);
+
+const digit$ = generate({
+  initialState: '0',
+  condition(bits) {
+    return bits.length <= 8;
+  },
+  iterate(bit) {
+    return bit + '0';
+  },
+  resultSelector(bit) {
+    return String(bit);
+  },
+});
+
+digit$.subscribe(console.log);
