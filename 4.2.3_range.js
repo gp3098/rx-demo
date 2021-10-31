e541120e68c955c8e89105f1a4fb3df462e2ece3
huangxinpeng <gp.3098@qq.com>
2021-10-31 20:02:43 +0800

    修改range样例

--- 4.2.3_range.js
+++ 4.2.3_range.js
@@ -1,4 +1,7 @@
 import { range } from 'rxjs';
 
 const source$ = range(1, 100);
-source$.subscribe(console.log);
+// source$.subscribe(console.log);
+
+const source2$ = range(1.5, 3);
+source2$.subscribe(console.log)
